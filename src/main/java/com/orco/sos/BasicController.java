d6c2751e6c24376a076621bd3db03a0b939d99db
Basile Bartolomeo <basilebartolomeo01@gmail.com>
2024-05-12 13:41:44 +0200

    first commit

---
+++ src/main/java/com/orco/sos/BasicController.java
@@ -0,0 +1,15 @@
+package com.orco.sos;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/dev")
+public class BasicController {
+
+    @GetMapping("/name")
+    public String getName() {
+        return "Gordon";
+    }
+}
